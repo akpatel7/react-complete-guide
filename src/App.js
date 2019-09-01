7e24e8a1eaba702355c7e8cd1af179141e574402
unknown <patel_amitk@hotmail.com>
2019-09-01 17:01:41 -0400

    passing args to handler as prop to change state

--- src/App.js
+++ src/App.js
@@ -12,31 +12,35 @@ class App extends Component {
     otherState: 'some other value'
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was Clicked');
     // Don't mutate state!!!
     // this.state.persons[0].name = 'Maximillian';
     this.setState({
       persons: [
-        { name: 'Maximillian', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
       ]
     })
   };
   render() {
+    /* Note: 
+      onClick={() => this.switchNameHandler('Maximmilan!!')}> can be ineffiecent multiple rerenders...
+      this.switchNameHandler.bind(this, 'Max!') is better
+    */
     return (
       <div className="App">
         <h1>Hi, I'm a React app</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Maximmilan!!')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler} >My hobbies: racing</Person>
+          click={this.switchNameHandler.bind(this, 'Max!')} >My hobbies: racing</Person>
         <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age} />
