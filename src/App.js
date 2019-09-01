3a719469e9ad21b12af9112084f4e2f03f580aae
unknown <patel_amitk@hotmail.com>
2019-09-01 16:13:46 -0400

    adding changing state in components

--- src/App.js
+++ src/App.js
@@ -5,16 +5,26 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max' , age: 28 },
-      { name: 'Manu' , age: 29 },
-      { name: 'Stephanie' , age: 26 }
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value'
   }
 
   switchNameHandler = () => {
-    console.log('Was Clicked');
+    // console.log('Was Clicked');
+    //Don't mutate state!!!
+    //this.state.persons[0].name = 'Maximillian';
+    this.setState({
+      persons: [
+        { name: 'Maximillian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
   }
+
 
   render() {
     return (
